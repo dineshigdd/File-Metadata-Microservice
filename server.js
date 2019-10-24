8dd50c2297adc3268cd0725337468fc5d5885561
Glitch (light-foxtail) <none>
2019-10-24 05:39:13 +0000

    😅😺 Checkpoint ./server.js:6079345/849

--- server.js
+++ server.js
@@ -3,10 +3,26 @@
 var express = require('express');
 var cors = require('cors');
 var multer = require('multer');
+var upload = multer({ dest: 'uploads/' })
 // require and use "multer"...
 
 var app = express();
-app.post('/api/fileanalyse')
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res, next) {
+  
+  
+})
+ 
+var cpUpload = upload.fields([{ name: 'upfile', maxCount: 1 }])
+app.post('/cool-profile', cpUpload, function (req, res, next) {
+  // req.files is an object (String -> Array) where fieldname is the key, and the value is array of files
+  //
+  // e.g.
+  //  req.files['avatar'][0] -> File
+  //  req.files['gallery'] -> Array
+  //
+  // req.body will contain the text fields, if there were any
+})
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
